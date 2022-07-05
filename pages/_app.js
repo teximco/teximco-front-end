51b086a0f25acfddc905137cd2c1511ffb6849f8
Ahad hossain <ahadxx99@gmail.com>
2022-07-05 14:27:12 -0500

    Brands edition

--- pages/_app.js
+++ pages/_app.js
@@ -3,23 +3,40 @@ import 'animate.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-inner-image-zoom/lib/InnerImageZoom/styles.min.css';
+import { useAmp } from 'next/amp'
 
+export const config = { amp: 'hybrid' }
 
 function MyApp({ Component, pageProps }) {
+
+  const isAmp = useAmp()
+
   return( <>
-    <Component {...pageProps} />
-    <ToastContainer 
-    position="top-center"
-    autoClose={5000}
-    hideProgressBar={false}
-    newestOnTop={false}
-    closeOnClick
-    rtl={false}
-    pauseOnFocusLoss
-    draggable
-    pauseOnHover
-    />
-    </>)
+     { isAmp ? 
+        
+        <p>AMP</p>
+       :
+
+      <div>
+         <Component {...pageProps} />
+       <ToastContainer 
+       position="top-center"
+       autoClose={5000}
+       hideProgressBar={false}
+       newestOnTop={false}
+       closeOnClick
+       rtl={false}
+       pauseOnFocusLoss
+       draggable
+       pauseOnHover
+       />
+      </div>
+      
+    }
+
+    </>
+    
+    )
 }
 
 export default MyApp
--- pages/components/brands.js
+++ pages/components/brands.js
@@ -3,43 +3,49 @@ import React from 'react';
 const Brands = () => {
     return (
        <div>
-        <h1 className='mt-20 text-center text-3xl font-bold'>Our Global Fashion Brand’s, Promotion and Advertising Brand’s</h1>
+        <h1 className='mt-20 text-center text-3xl font-bold'>Complience and Certifications</h1>
          <div className='brands'>
             <img src="/logos/Untitled-3.png" alt="" />
             <img src="/logos/Untitled-4.png" alt="" />
-            <img src="/logos/Untitled-5.png" alt="" />
-            <img src="/logos/Untitled-6.png" alt="" />
             <img src="/logos/Untitled-7.png" alt="" />
             <img src="/logos/Untitled-8.png" alt="" />
-            <img src="/logos/Untitled-9.png" alt="" />
-            <img src="/logos/Untitled-10.png" alt="" />
             <img src="/logos/Untitled-11.png" alt="" />
             <img src="/logos/Untitled-12.png" alt="" />
             <img src="/logos/Untitled-13.png" alt="" />
-            <img src="/logos/Untitled-14.png" alt="" />
             <img src="/logos/Untitled-15.png" alt="" />
-            <img src="/logos/Untitled-16.png" alt="" />
-            <img src="/logos/Untitled-17.png" alt="" />
-            <img src="/logos/Untitled-18.png" alt="" />
-            <img src="/logos/Untitled-19.png" alt="" />
-            <img src="/logos/Untitled-20.png" alt="" />
-            <img src="/logos/Untitled-21.png" alt="" />
-            <img src="/logos/Untitled-22.png" alt="" />
-            <img src="/logos/Untitled-23.png" alt="" />
-            <img src="/logos/Untitled-24.png" alt="" />
-            <img src="/logos/Untitled-25.png" alt="" />
-            <img src="/logos/Untitled-26.png" alt="" />
-            <img src="/logos/Untitled-27.png" alt="" />
-            <img src="/logos/Untitled-28.png" alt="" />
-            <img src="/logos/Untitled-29.png" alt="" />
-            <img src="/logos/Untitled-30.png" alt="" />
-            <img src="/logos/Untitled-31.png" alt="" />
-            <img src="/logos/Untitled-32.png" alt="" />
-            <img src="/logos/Untitled-33.png" alt="" />
-            <img src="/logos/Untitled-34.png" alt="" />
-            <img src="/logos/Untitled-35.png" alt="" />
-            <img src="/logos/Untitled-36.png" alt="" />
+
+
         </div>
+        <h1 className='mt-20 text-center text-3xl font-bold'>Our Global Fashion Brand’s, Promotion and Advertising Brand’s</h1>
+         <div className='brands'>
+         <img src="/logos/Untitled-16.png" alt="" />
+        <img src="/logos/Untitled-27.png" alt="" />
+        <img src="/logos/Untitled-17.png" alt="" />
+        <img src="/logos/Untitled-34.png" alt="" />
+        <img src="/logos/Untitled-22.png" alt="" />
+        <img src="/logos/Untitled-26.png" alt="" />
+        <img src="/logos/Untitled-18.png" alt="" />
+        <img src="/logos/Untitled-19.png" alt="" />
+        <img src="/logos/Untitled-25.png" alt="" />
+        <img src="/logos/Untitled-30.png" alt="" />
+        <img src="/logos/Untitled-23.png" alt="" />
+        <img src="/logos/Untitled-29.png" alt="" />
+        <img src="/logos/Untitled-6.png" alt="" />
+        <img src="/logos/Untitled-24.png" alt="" />
+        <img src="/logos/Untitled-9.png" alt="" />
+        <img src="/logos/Untitled-20.png" alt="" />
+        <img src="/logos/Untitled-10.png" alt="" />
+        <img src="/logos/Untitled-28.png" alt="" />
+        <img src="/logos/Untitled-14.png" alt="" />
+        <img src="/logos/Untitled-21.png" alt="" />
+        <img src="/logos/Untitled-5.png" alt="" />
+        <img src="/logos/Untitled-33.png" alt="" />
+        <img src="/logos/Untitled-31.png" alt="" />
+        <img src="/logos/Untitled-35.png" alt="" />
+        <img src="/logos/Untitled-32.png" alt="" />
+        <img src="/logos/Untitled-36.png" alt="" />
+        </div>
+
        </div>
     );
 };
